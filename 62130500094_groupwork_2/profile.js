49c9542522fb94c7ccec18a9c5f73fef9d472725
Sittichok Pintakul <sittichok.ping@mail.kmutt.ac.th>
2021-02-03 11:18:35 +0700

    [update] remove unnecessary comments

--- 62130500094_groupwork_2/profile.js
+++ 62130500094_groupwork_2/profile.js
@@ -10,13 +10,8 @@ const app = {
             rating_num: 9.9,
             button1: "Call",
             button2: "Chat"
-            // msg: 'Hello, Vue3',
-            // shown: true,
-            // url: "https://sit.kmutt.ac.th"
         }
     }
-    // created() { console.log('message is '+ this.msg) },
-    // updated() { console.log('message is changed to '+ this.msg) }
 }
 
 mountedApp = Vue.createApp(app).mount('#app')
